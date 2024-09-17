2bea25b94ea8a005a9d1bfb0b26c159e749a1b44
DeVoresyah Arest <de.voresyah@gmail.com>
2024-09-17 10:29:11 +0700

    fix: remove footer

--- components/layout/footer/footer.jsx
+++ components/layout/footer/footer.jsx
@@ -8,14 +8,7 @@ const Footer = () => {
     <>
       <footer className="relative mt-12 mb-20 left-1/2 bottom-2 transform -translate-x-1/2 w-[200px]">
         <p className="mt-10 mb-4 text-black text-semibold text-center text-lg">
-          Made by{' '}
-          <Link
-            className="text-blue-800"
-            target="_blank"
-            href="https://twitter.com/NerdyProgramme2"
-          >
-            @urdadx
-          </Link>
+          &copy;2024 Bisa.ae, All right reserved.
         </p>
       </footer>
       {isMobile && <div className="h-[40px] mb-24" />}
